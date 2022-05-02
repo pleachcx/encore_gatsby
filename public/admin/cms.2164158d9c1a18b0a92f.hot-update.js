3de0227a081fff121343f9c3b4e21a2a54d20dc6
Patrick Leach <pl@leach.cx>
2022-05-02 11:01:56 +1000

    max image height

---
+++ public/admin/cms.2164158d9c1a18b0a92f.hot-update.js
@@ -0,0 +1,22 @@
+webpackHotUpdate("cms",{
+
+/***/ "./src/components/Content.css":
+/*!************************************!*\
+  !*** ./src/components/Content.css ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+    if(true) {
+      // 1651453237696
+      var cssReload = __webpack_require__(/*! ./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ "./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js")(module.i, {"hmr":true,"locals":false});
+      module.hot.dispose(cssReload);
+      module.hot.accept(undefined, cssReload);
+    }
+  
+
+/***/ })
+
+})
+//# sourceMappingURL=cms.2164158d9c1a18b0a92f.hot-update.js.map
---
+++ public/admin/cms.fed9d3659e6654845de7.hot-update.js
@@ -0,0 +1,22 @@
+webpackHotUpdate("cms",{
+
+/***/ "./src/components/Content.css":
+/*!************************************!*\
+  !*** ./src/components/Content.css ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+    if(true) {
+      // 1651453212062
+      var cssReload = __webpack_require__(/*! ./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ "./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js")(module.i, {"hmr":true,"locals":false});
+      module.hot.dispose(cssReload);
+      module.hot.accept(undefined, cssReload);
+    }
+  
+
+/***/ })
+
+})
+//# sourceMappingURL=cms.fed9d3659e6654845de7.hot-update.js.map
