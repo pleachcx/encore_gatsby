18fa76e9a788ead0aa32dbf7a7e7a78015bf5505
Patrick Leach <pl@leach.cx>
2022-05-25 04:37:34 +1000

    cookies

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,41 @@ module.exports = {
     siteUrl: 'https://encorehomestaging.com.au'
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-gdpr-cookies`,
+      options: {
+        googleAnalytics: {
+          trackingId: 'G-ZSZW8SNEBZ', // leave empty if you want to disable the tracker
+          cookieName: 'gatsby-gdpr-google-analytics', // default
+          anonymize: true, // default
+          allowAdFeatures: false // default
+        },
+        googleTagManager: {
+          trackingId: 'GTM-WBJCNMC', // leave empty if you want to disable the tracker
+          cookieName: 'gatsby-gdpr-google-tagmanager', // default
+          dataLayerName: 'dataLayer', // default
+        },
+        facebookPixel: {
+          pixelId: 'YOUR_FACEBOOK_PIXEL_ID', // leave empty if you want to disable the tracker
+          cookieName: 'gatsby-gdpr-facebook-pixel', // default
+        },
+        tikTokPixel: {
+          pixelId: 'YOUR_TIKTOK_PIXEL_ID', // leave empty if you want to disable the tracker
+          cookieName: 'gatsby-gdpr-tiktok-pixel', // default
+        },
+        hotjar: {
+          hjid: 'YOUR_HOTJAR_ID',
+          hjsv: 'YOUR_HOTJAR_SNIPPET_VERSION',
+          cookieName: 'gatsby-gdpr-hotjar', // default
+        },
+        linkedin: {
+          trackingId: 'YOUR_LINKEDIN_TRACKING_ID', // leave empty if you want to disable the tracker
+          cookieName: 'gatsby-gdpr-linked-in', // default
+        },
+        // defines the environments where the tracking should be available  - default is ["production"]
+        environments: ['production', 'development']
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-yaml',
     { 
