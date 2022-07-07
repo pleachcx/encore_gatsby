de4cac3443d5c20ff636be168cb047ff09b4de63
Patrick Leach <pl@leach.cx>
2022-07-07 23:09:52 +1000

    pixel

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
           dataLayerName: 'dataLayer', // default
         },
         facebookPixel: {
-          pixelId: '708083663867805', // leave empty if you want to disable the tracker
+          pixelId: '781183306373469', // leave empty if you want to disable the tracker
           cookieName: 'gatsby-gdpr-facebook-pixel', // default
         },
         tikTokPixel: {
